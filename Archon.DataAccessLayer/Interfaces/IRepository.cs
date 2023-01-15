6855aa6d0dbcf7fa3fa9ddc3dcefe96dc2e5c1dd
Jonathan Espinal <macwinandios@gmail.com>
2023-01-15 14:20:06 -0500

    DataAccessLayer, Tests, WebApi

---
+++ Archon.DataAccessLayer/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archon.DataAccessLayer
+{
+    //public interface IRepository<T> where T : class
+    //{
+    //    //Task<bool> AddUserAsync(T model);
+    //    //void AddUserAsync();
+    //    Task<bool> UpdateUserAsync(T model);
+    //    //Task<bool> DeleteUserAsync(string id);
+    //    Task<bool> GetUserAsync(string id);
+    //    Task<IEnumerable<T>> GetAllUsersAsync (bool forceRefresh = false);
+    //}
+}
---
+++ Archon.DataAccessLayer/Models/EmployeeTimeModel.cs
@@ -0,0 +1,23 @@
+﻿using Archon.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archon.DataAccessLayer.Models
+{
+    public class EmployeeTimeModel : IEmployeeTimeModel
+    {
+        public DateTime CurrentTime { get; set; }
+        public DateTime ClockedInAt { get; set; }
+        public DateTime ClockedOutAt { get; set; }
+        public DayOfWeek DateClockedIn { get; set; }
+        public float HourlyWage { get; set; }
+        public float WagesEarned { get; set; }
+        public DayOfWeek DateClockedOut { get; set; }
+        public TimeSpan DurationOfClockIn { get; set; }
+        public TimeSpan TotalTimeClockedInToday { get; set; }
+        public TimeSpan TotalTimeClockedInThisWeek { get; set; }
+        public string Username { get; set; }
+
+    }
+}
