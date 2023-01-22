7af9d53956e4b7a469857cb245df857fa21e23f2
Jonathan Espinal <105177727+macwinandios@users.noreply.github.com>
2023-01-22 14:57:59 -0500

    Added PCL.
    Archon.DataAccessLayer PCL added.

---
+++ Archon.DataAccessLayer/Repositories/AdminAssignTaskRepository.cs
@@ -0,0 +1,41 @@
+﻿using Archon.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archon.DataAccessLayer.Repositories
+{
+    public class AdminAssignTaskRepository : IRepository<IAdminAssignTaskViewModel>
+    {
+        public Task DeleteAsync(IAdminAssignTaskViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IAdminAssignTaskViewModel>> GetAllAsync(IAdminAssignTaskViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByIdOrUsername(IAdminAssignTaskViewModel viewModel, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByIdOrUsername(IAdminAssignTaskViewModel viewModel, string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostAsync(IAdminAssignTaskViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PutAsync(IAdminAssignTaskViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
